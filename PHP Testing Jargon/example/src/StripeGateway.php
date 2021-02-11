a78b70db18d47749277e3420b6e1c7c65d980a0a
Darren-San <jpineda.nulglass@gmail.com>
2021-02-11 15:25:39 +0100

    finished section 2

---
+++ PHP Testing Jargon/example/src/StripeGateway.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class StripeGateway implements Gateway
+{
+  public function create()
+  {
+    var_dump('Slow HTTP request in progress.');
+  }
+}
---
+++ PHP Testing Jargon/example/tests/FakeGateway.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+use App\Gateway;
+
+class FakeGateway implements Gateway;
+{
+  public function create()
+  {
+  }
+}
